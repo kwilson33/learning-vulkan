ccf90aeeebcb35be2c64408578a3be1bb09fb3cd
Kevin Wilson <kwilson33@wisc.edu>
2020-12-12 14:25:14 -0600

    Halfway through Instance part of Drawing a triangle

--- DrawingATriangle/DrawingATriangle_Setup/main.cpp
+++ DrawingATriangle/DrawingATriangle_Setup/main.cpp
@@ -27,11 +27,58 @@ private:
     
     // private class member to store reference to window
     GLFWwindow* window;
+    // private class member to store the Vulkan instance
+    VkInstance instance;
+
+    // The instance is connection b/w your app and the Vulkan library
+    void createInstance() {
+        // first, fill in struct. Data is technicall optional, but may provide some useful information to the driver in order to optimize our specific app.
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pNext = nullptr;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+
+        // this next struct is NOT optional and tells the Vulkan driver which global extensions and validation layers we want to use.
+        VkInstanceCreateInfo createInfo{};  
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // GLFW has built-in func that returns the extensions it needs for Vulkan to interface with the window system.
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        // Use the built-in func to get # extensions, and add it, and the extension names, to the struct
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // Deternines the global validation layers to enable.
+        createInfo.enabledLayerCount = 0;
+
+        // We've now specified everything Vulkan needs to create an instance
+        // The general pattern for object creation function paramers is
+        // 1) Pointer to struct with creation info
+        // 2) Pointer to custom allocator callbacks, always nullptr for this tutorial
+        // 3) Pointer to the variable that store the handle to the new object
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+    }
 
     // calls funcs to initiate Vulkan objects
     void initVulkan() {
+        // Very first thing to init Vulkan library is by creating an instance.
+        createInstance();
 
     }
+
+    
 
     
     // initialize GLFW and create a window
